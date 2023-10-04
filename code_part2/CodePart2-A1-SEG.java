f4b747351aeb98ad390118c275bad1040d370dba
Seif Al Qutob <113942342+seif739@users.noreply.github.com>
2023-10-03 23:48:00 -0400

    Add files via upload
    not finished yet

---
+++ code_part2/CodePart2-A1-SEG.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.Vector;
+import java.util.Random;
+import java.util.Iterator;
+
+public class CollectionExecutingTime{
+
+
+    public static long getTotalVectors(Vector<Integer> vector){
+        
+    }
+
+    public static long getTotalArray(int[] array){
+        long total = 0;
+        for(int i = 0; i < array.length; i++){ 
+            total = total + array[i];
+        }
+        return total;
+    }
+
+    public static long getTotalListArray(ArrayList<Integer> listOfArray){
+    
+    }
+
+    public static void initializingContents(int[] array, int size, Vector<Integer> vector, ArrayList<Integer> listOfArray){
+        Random randomize = new Random(); //creating a randomiser
+        for(int j = 0; j < size; j++){
+            int randomNumber = randomize.nextInt(1000000); //Generates random numbers from bounds from 0 till 1000000
+            array[j] = randomNumber;
+            vector.addElement(randomNumber);
+            listOfArray.add(randomNumber);
+        }
+    }
+
+    public static void main(String[] args){
+
+        long vectorElapsedTime; //keeps track of the elapsed time for vectors
+        long arrayElapsedTime; //keeps track of the elapsed time for the array
+	    long listOfArrayElapsedTime;//keeps track of the elapsed time for the listOfArray
+
+        final int size = 1000000; //NOT SURE IF THIS IS THE RIGHT SIZE BUT CHANGE IT IF U HAVE TO RAIYAN !!!!!!!!!
+	    
+
+        int[] array = new int[size]; //new array of size 'size' (1,000,000)
+	    Vector<Integer> vector = new Vector<Integer>(); // new Vector
+	    ArrayList<Integer> listOfArray = new ArrayList<Integer>(); //new arrayList
+	
+	    initializingContents(array, size, vector, listOfArray); //initializing the contents with random numbers thanks to the initializingContents() method
+	    long beginTimer = System.nanoTime(); //start time
+        getTotalVectors(vector);
+        vectorElapsedTime = System.nanoTime();
+        getTotalArray(array);
+        arrayElapsedTime = System.nanoTime();
+        getTotalListOfArray(listOfArray);
+        listOfArrayElapsedTime = System.nanoTime();
+
+        long firstTimeNano = arrayElapsedTime - beginTimer;
+        long secondTimeNano = listOfArrayElapsedTime - arrayElapsedTime;
+        long thirdTimeNano = vectorElapsedTime - arrayElapsedTime;
+
+        long firstTimeSec = TimeUnit.NANOSECONDS.toSeconds(firstTimeNano);
+        long secondTimeSec = TimeUnit.NANOSECONDS.toSeconds(secondTimeNano);
+        long thirdTimeSec = TimeUnit.NANOSECONDS.toSeconds(thirdTimeSec);
+
+    }
+}
