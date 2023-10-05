19c9f4f78f585954216a2ec3d8272a8b347bf442
Seif Al Qutob <113942342+seif739@users.noreply.github.com>
2023-10-04 21:59:24 -0400

    Update CollectionExecutingTime.java

--- code_part2/CollectionExecutingTime.java
+++ code_part2/CollectionExecutingTime.java
@@ -8,11 +8,11 @@ public class CollectionExecutingTime{
 
     public static long getTotalVectors(Vector<Integer> vector){
         long total = 0;
-        Iterator<Integer> iterator = vector.iterator();
-        while(iterator.hasNext()){
-            total = total + (long)iterator.next();
+        Iterator<Integer> iterator = vector.iterator(); //Using an Iterator for the Vector
+        while(iterator.hasNext()){ //as long as this condition is true it will continue to iterate
+            total = total + iterator.next();
         }
-        return total;
+        return (total);
     }
 
     public static long getTotalArray(int[] array){
@@ -20,16 +20,16 @@ public class CollectionExecutingTime{
         for(int i = 0; i < array.length; i++){ 
             total = total + array[i];
         }
-        return total;
+        return (total);
     }
 
     public static long getTotalListArray(ArrayList<Integer> listOfArray){
         long total = 0;
-        Iterator<Integer> iterator = listOfArray.iterator();
-        while(iterator.hasNext()){
-            total = total + (long)iterator.next();
+        Iterator<Integer> iterator = listOfArray.iterator(); // Use a an iterator for ArrayList.
+        while(iterator.hasNext()){ //as long as this condition is true it will continue to iterate
+            total = total + iterator.next();
         }
-        return total;
+        return (total);
     }
 
     public static void initializingContents(int[] array, int size, Vector<Integer> vector, ArrayList<Integer> listOfArray){
@@ -44,11 +44,11 @@ public class CollectionExecutingTime{
 
     public static void main(String[] args){
 
-        long vectorElapsedTime; //keeps track of the elapsed time for vectors
-        long arrayElapsedTime; //keeps track of the elapsed time for the array
-	    long listOfArrayElapsedTime;//keeps track of the elapsed time for the listOfArray
+        long vectorTime; //keeps track of the elapsed time for vectors
+        long arrayTime; //keeps track of the elapsed time for the array
+	    long listOfArrayTime;//keeps track of the elapsed time for the listOfArray
 
-        final int size = 1000000; 
+        final int size = 10000000; //This will be the size used by all the vector, array and arraylist
 	    
 
         int[] array = new int[size]; //new array of size 'size' (1,000,000)
@@ -58,23 +58,28 @@ public class CollectionExecutingTime{
 	    initializingContents(array, size, vector, listOfArray); //initializing the contents with random numbers thanks to the initializingContents() method
 	    long beginTimer = System.nanoTime(); //start time
         getTotalVectors(vector);
-        vectorElapsedTime = System.nanoTime();
+        vectorTime = System.nanoTime();
         getTotalArray(array);
-        arrayElapsedTime = System.nanoTime();
+        arrayTime = System.nanoTime();
         getTotalListArray(listOfArray);
-        listOfArrayElapsedTime = System.nanoTime();
+        listOfArrayTime = System.nanoTime();
 
-        long firstTimeNano = arrayElapsedTime - beginTimer;
-        long secondTimeNano = listOfArrayElapsedTime - arrayElapsedTime;
-        long thirdTimeNano = arrayElapsedTime - vectorElapsedTime ;
+        long thirdTimeNano = arrayTime - vectorTime ; //calculating the elapsed time (in ns) for vector
+        long firstTimeNano = arrayTime - beginTimer; //calculating the elapsed time (in ns) for array
+        long secondTimeNano = listOfArrayTime - arrayTime; //calculating the elapsed time (in ns) for array of list
 
-        //long firstTimeSec = TimeUnit.NANOSECONDS.toSeconds(firstTimeNano);
-        //long secondTimeSec = TimeUnit.NANOSECONDS.toSeconds(secondTimeNano);
-        //long thirdTimeSec = TimeUnit.NANOSECONDS.toSeconds(thirdTimeNano);
+        System.out.println("Vector size is: " + size); //Printing the size for vector
+        System.out.println("time for vector is: " + thirdTimeNano +" ns"); //Printing the time for the vector
+        
+        System.out.println("--------------------------------------------");
 
-        System.out.println("Array: " + firstTimeNano);
-        System.out.println("ArrayList: " +secondTimeNano);
-        System.out.print("Vector: " +thirdTimeNano);
+        System.out.println("Array size is: " + size); //Printing the size for array
+        System.out.println("time for array is: " + firstTimeNano + " ns"); //Printing the time for the array
+
+        System.out.println("--------------------------------------------");
+
+        System.out.println("List of array size is: " + size); //Printing the size for list of array
+        System.out.println("time for list of array is: " + secondTimeNano +" ns"); //Printing the time for the list of array
 
     }
 }
